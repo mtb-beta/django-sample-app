24bee7673c95825a76d78dfa7cdb1a3df5c16b86
mtb-beta <mtb.toya0403@gmail.com>
2019-07-23 12:45:40 +0700

    add OneToOneField

--- sample_prj/sample1/models.py
+++ sample_prj/sample1/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
+class MyDetailModel(models.Model):
+    name = models.CharField(max_length=15, default="")
+
+    def __str__(self):
+        return self.name
+
+
 class MyModel(models.Model):
     name = models.CharField(max_length=10, default="")
+    detail = models.OneToOneField(MyDetailModel, default="")
 
     def __str__(self):
         return self.name
